84a84514c2b77df3f0f09a519b458e4333a968c1
vinogandamalla <vinodgndmalla@gmail.com>
2016-03-29 17:24:27 +0530

    server side routing

--- src/clj/loginform/core.clj
+++ src/clj/loginform/core.clj
@@ -6,23 +6,73 @@
             [compojure.response :refer [render]]
             [clojure.java.io :as io]
             [bouncer.core :as b]
-            [bouncer.validators :as v]))
+            [bouncer.validators :as v]
+            [ring.middleware.json :as ring-json]
+            [compojure.route :as route]
+            [ring.util.response	:as rr]
+           ))
 
 ;; Simple function that works as controller
 ;; It should return a proper response. In our
 ;; case it returns a content of static index.html.
+
+(def in-db {:users [{:user "user1" :password "techno" :email-id "user1@gmail.com"}
+                   {:user "user2" :password "identity" :email-id "user2@gmail.com"}
+                   {:user "user3" :password "ti" :email-id "user3@gmail.com"}
+                   {:user "user4" :password "tid" :email-id "user4@gmail.com"}]})
+
+
+(defn find-in [in-db email-id]
+  (println  email-id)
+  (first
+   (filter (fn[user](= (:email-id user) email-id))
+           (:users in-db))))
+
+
 (defn home
   [req]
   (render (io/resource "index.html") req))
 
 ;; Routes definition
-(defroutes app
+(defroutes app-routes
   (GET "/" [] home)
+  (POST "/authenticate" {body :body}
+        (let [usr (find-in in-db (:email-id body))]
+          (println (:email-id body))
+          (rr/content-type
+           (cond (or (nil? usr)
+                     (not= (:password usr)(:password body))) (rr/status {:message "invalid credentials"} 404)
+                 :else (rr/response (dissoc usr :password))) "application/json")))
+ 
+  (POST "/changepassword" {body :body}
+    (let [usr (find-in in-db (:email-id body))]
+      (println (:email-id body))
+      (rr/content-type
+       (cond (or (nil? usr)
+                 (not= (:password usr)(:password body))) (rr/status {:message "invalid user"} 404)
+             :else (rr/response (dissoc usr :password))) "application/json")))
+  
+
+  (POST "/forgotpassword" {body :body}
+    (let [usr (find-in in-db (:email-id body))]
+      (println (:email-id body))
+      (rr/content-type
+       (cond (or (nil? usr)
+                 (not= (:password usr)(:password body))) (rr/status {:message "invalid user"} 404)
+             :else (rr/response (dissoc usr :password :user))) "application/json")))
+  
   (route/resources "/static")
   (route/not-found "<h1>Page not found</h1>"))
+
+(def app
+  (-> app-routes
+      (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
+      (ring-json/wrap-json-body {:keywords? true})
+      (ring-json/wrap-json-response)))
+
 
 ;; Application entry point
 (defn -main
   [& args]
-  (let [app (wrap-defaults app site-defaults)]
-    (run-jetty {:ring-handler app :port 8081})))
+  (run-jetty {:ring-handler app :port 8081}))
+
--- src/cljs/loginform/core.cljs
+++ src/cljs/loginform/core.cljs
@@ -48,101 +48,320 @@
    [:div.col-sm-7 [mytextbox type id]]
    ])
 
+(defn mytextbox1 [id finbool fintext]
+  [:input.form-control {:type "text"
+                        :id id
+                        :value @fintext
+                        :on-change (fn [event] (reset! fintext (-> event .-target .-value)))
+                        :on-key-press (fn [event]
+                                        (when (= 13 (.-charCode event))
+                                          (reset! finbool false)))
+                        :on-dbl-click (fn [event]
+                                        (reset! finbool false))
+            
+                        }])
 
-(defn myradiobutton [name value]
-  [:input {:type "radio" :name name :value value} value])
+
+
+(defn myradiobutton [name value genderbool gendertext]
+  [:input {:type "radio" :name name :value value :on-click (fn [event]
+                                                              (reset! genderbool false))} value])
+
+(defn myradiobutton1 [name value label]
+  [:input {:type "radio" :name name :value value } [:label label]])
+
+(defn myselect []
+  [:select.form-control
+    [:option {:value "Select"} "Select"]
+    [:option {:value "B"} "B"]
+    [:option {:value "C"} "C"]
+    [:option {:value "D"} "D"]
+    [:option {:value "E"} "E"]
+    [:option {:value "F"} "F"]
+    [:option {:value "G"} "G"]])
 
 
 
 (defn personaldetails []
-  [:div.form-group [:label [:h2 "Personal Details"]]
-   [:div.row 
-    [:div.col-md-5
-     [labelandtextbox "First Name*" "text" "fn"]]
-    [:div.col-md-5
-     [labelandtextbox "Father's Name/Spouse Name*" "text" "ln"]]]
-   [:div.row
-    [:div.col-md-5
-     [labelandtextbox "Last Name*"]]
-    [:div.col-md-5
-     [:div.row
-      [:div.col-sm-5 [:label "Gender"]]
-      [:div.col-sm-7
+  (let [finbool (reagent/atom true)
+        fintext (reagent/atom "abc")
+        fanbool (reagent/atom true)
+        fantext (reagent/atom "gvk")
+        lnbool (reagent/atom true)
+        lntext (reagent/atom "def")
+        nationbool (reagent/atom true)
+        nationtext (reagent/atom "India")
+        genderbool (reagent/atom true)
+        gendertext (reagent/atom "")
+       ]
+    (fn []
+      [:div.form-group [:label [:h2 "Personal Details"]]
+       [:hr]
+       [:hr]
        [:div.row
-        [:div.col-sm-6 [myradiobutton "Gender" "Male"]]
-        [:div.col-sm-6 [myradiobutton "Gender" "Female"]]]]]]]
-   [:div.row
-    [:div.col-md-5
-     [labelandtextbox "Date of Birth*" "date" "dob"]]
-    [:div.col-md-5
-     [:div.row
-      [:div.col-sm-5 [:label "Marital Status"]]
-      [:div.col-sm-7
+        [:div.col-md-5
+         [:div.row {:on-mouse-enter (fn [event] (reset! finbool true))
+                    :on-mouse-leave (fn [event]
+                                      (reset! finbool false))}
+          [:div.col-sm-5 [:label "First Name*"]]
+          [:div.col-sm-7
+           (if (= @finbool true)
+             [mytextbox1 "fn" finbool fintext]
+             [:span  @fintext])]]]
+        [:div.col-md-5
+         [:div.row {:on-mouse-enter (fn [event] (reset! fanbool true))
+                    :on-mouse-leave (fn [event]
+                                      (reset! fanbool false))}
+          [:div.col-sm-5 [:label "Father's Name/Spouse Name*"]]
+          [:div.col-sm-7
+           (if (= @fanbool true)
+             [mytextbox1 "fan" fanbool fantext]
+             [:span @fantext])]]]]
        [:div.row
-        [:div.col-sm-6 [myradiobutton "maritalstatus" "Single"]]
-        [:div.col-sm-6 [myradiobutton "maritalstatus" "Married"]]]]]]]
-   [:div.row
-    [:div.col-md-5]
-    [:div.col-md-5
-     [labelandtextbox "Nationality" "text" "Nationality"]]]
-   [:div.row
-    [:div.col-md-5
-     [:div.row
-      [:div.col-sm-5 [:label "Status"]]
-      [:div.col-sm-7 [myradiobutton "status" "Resident Individual"]]]]
-    [:div.col-md-7
-     [:div.row
-      [:div.col-sm-6 [myradiobutton "status" "Non Resident Individual"]]
-      [:div.col-sm-6 [myradiobutton "status" "Foreign National(Passport copy Mandatory)"]]]]]])
-
+        [:div.col-md-5
+         [:div.row {:on-mouse-enter (fn [event] (reset! lnbool true))
+                    :on-mouse-leave (fn [event]
+                                      (reset! lnbool false))}
+          [:div.col-sm-5 [:label "Last Name*"]]
+          [:div.col-sm-7
+           (if (= @lnbool true)
+             [mytextbox1 "fn" lnbool lntext]
+             [:span  @lntext])]]]
+        [:div.col-md-5
+         [:div.row
+          [:div.col-sm-5 [:label "Gender"]]
+          [:div.col-sm-7
+           (if (= @genderbool true)
+             [:div.row
+              [:div.col-sm-6 [myradiobutton "Gender" "Male"]]
+              [:div.col-sm-6 [myradiobutton "Gender" "Female"]]]
+             [:span @gendertext])]]]]
+       [:div.row
+        [:div.col-md-5
+         [labelandtextbox "Date of Birth*" "date" "dob"]]
+        [:div.col-md-5
+         [:div.row
+          [:div.col-sm-5 [:label "Marital Status"]]
+          [:div.col-sm-7
+           [:div.row
+            [:div.col-sm-6 [myradiobutton "maritalstatus" "Single"]]
+            [:div.col-sm-6 [myradiobutton "maritalstatus" "Married"]]]]]]]
+       [:div.row
+        [:div.col-md-5
+         [:div.row {:on-mouse-enter (fn [event] (reset! nationbool true))
+                    :on-mouse-leave (fn [event]
+                                      (reset! nationbool false))}
+          [:div.col-sm-5 [:label "Nationality"]]
+          [:div.col-sm-7
+           (if (= @nationbool true)
+             [mytextbox1 "fn" nationbool nationtext]
+             [:span  @nationtext])]]]]
+       [:div.row
+        [:div.col-md-5
+         [:div.row
+          [:div.col-sm-5 [:label "Status"]]
+                   [:div.col-sm-7 [myradiobutton "status" "Resident Individual"]]]]
+        [:div.col-md-5
+         [:div.row
+          [:div.col-sm-5[myradiobutton "status" "Non Resident Individual"]]
+          [:div.col-sm-7 [myradiobutton "status" "Foreign National(Passport copy Mandatory)"]]]]]
+      ])))
 
 
 (defn addressdetails []
-  [:div.form-group [:label [:h2 "Address Details"]]
-   [:div.row
-    [:div.col-md-5
-     [labelandtextbox "House/Flat No" "text" "h.no"]]
-    [:div.col-md-5
-     [labelandtextbox "Land Mark" "text" "landmark"]]]
-   [:div.row
-    [:div.col-md-5
-     [labelandtextbox "Street/Road no" "text" "street"]]
-    [:div.col-md-5
-     [labelandtextbox "Mobile No" "text" "mobileno"]]]
-   [:div.row
-    [:div.col-md-5
-     [labelandtextbox "City/Town/Village*" "text" "city"]]
-    [:div.col-md-5
-     [labelandtextbox "Land line" "text" "landline"]]]
-   [:div.row
-    [:div.col-md-5
-     [labelandtextbox "House/Flat No." "text" "h.no"]]
-    [:div.col-md-5
-     [labelandtextbox "Email-id" "email" "email"]]]
-     ])
+  (let [hnbool (reagent/atom true)
+        hntext (reagent/atom "1-97")
+        ldmbool (reagent/atom true)
+        ldmtext (reagent/atom "avenue")
+        streetbool (reagent/atom true)
+        streettext (reagent/atom "7th lane")
+        mobilebool (reagent/atom true)
+        mobiletext (reagent/atom "9247358308")
+        citybool (reagent/atom true)
+        citytext (reagent/atom "hyderabad")
+        landlinebool (reagent/atom true)
+        landlinetext (reagent/atom "040-82099")
+        emailbool (reagent/atom true)
+        emailtext (reagent/atom "gvk@gmail.com")
+        telbool (reagent/atom true)
+        teltext (reagent/atom "123456")
+        pinbool (reagent/atom true)
+        pintext (reagent/atom 500085)
+        faxbool (reagent/atom true)
+        faxtext (reagent/atom "012010")]
+    (fn []
+      [:div.form-group [:label [:h2 "Address Details"]]
+       [:hr]
+       [:hr]
+       [:div.row
+        [:div.col-md-5
+         [:div.row {:on-mouse-enter (fn [event] (reset! hnbool true))
+                    :on-mouse-leave (fn [event]
+                                      (reset! hnbool false))}
+          [:div.col-sm-5 [:label "House/Flat No"]]
+          [:div.col-sm-7
+           (if (= @hnbool true)
+             [mytextbox1 "hno" hnbool hntext]
+             [:span  @hntext])]]]
+        [:div.col-md-5
+         [:div.row {:on-mouse-enter (fn [event] (reset! ldmbool true))
+                    :on-mouse-leave (fn [event]
+                                      (reset! ldmbool false))}
+          [:div.col-sm-5 [:label "Landmark"]]
+          [:div.col-sm-7
+           (if (= @ldmbool true)
+             [mytextbox1 "ldm" ldmbool ldmtext]
+             [:span @ldmtext])]]]]
+       [:div.row
+        [:div.col-md-5
+         [:div.row {:on-mouse-enter (fn [event] (reset! streetbool true))
+                    :on-mouse-leave (fn [event]
+                                      (reset! streetbool false))}
+          [:div.col-sm-5 [:label "Street/Road Name"]]
+          [:div.col-sm-7
+           (if (= @streetbool true)
+             [mytextbox1 "streetno" streetbool streettext]
+             [:span  @streettext])]]]
+        [:div.col-md-5
+         [:div.row {:on-mouse-enter (fn [event] (reset! mobilebool true))
+                    :on-mouse-leave (fn [event]
+                                      (reset! mobilebool false))}
+          [:div.col-sm-5 [:label "Mobile No"]]
+          [:div.col-sm-7
+           (if (= @mobilebool true)
+             [mytextbox1 "mobileno" mobilebool mobiletext]
+             [:span @mobiletext])]]]]
+       [:div.row
+        [:div.col-md-5
+         [:div.row {:on-mouse-enter (fn [event] (reset! citybool true))
+                    :on-mouse-leave (fn [event]
+                                      (reset! citybool false))}
+          [:div.col-sm-5 [:label "City/Town/Village*"]]
+          [:div.col-sm-7
+           (if (= @citybool true)
+             [mytextbox1 "city" citybool citytext]
+             [:span  @citytext])]]]
+        [:div.col-md-5
+         [:div.row {:on-mouse-enter (fn [event] (reset! landlinebool true))
+                    :on-mouse-leave (fn [event]
+                                      (reset! landlinebool false))}
+          [:div.col-sm-5 [:label "Landline"]]
+          [:div.col-sm-7
+           (if (= @landlinebool true)
+             [mytextbox1 "landline" landlinebool landlinetext]
+             [:span @landlinetext])]]]]
+       [:div.row
+        [:div.col-md-5
+         [:div.row
+          [:div.col-sm-5 [:label "District*"]]
+          [:div.col-sm-7 [myselect]]]]
+        [:div.col-md-5
+         [:div.row {:on-mouse-enter (fn [event] (reset! emailbool true))
+                    :on-mouse-leave (fn [event]
+                                      (reset! emailbool false))}
+          [:div.col-sm-5 [:label "Email id"]]
+          [:div.col-sm-7
+           (if (= @emailbool true)
+             [mytextbox1 "email" emailbool emailtext]
+             [:span  @emailtext])]]]]
+       [:div.row
+        [:div.col-md-5
+         [:div.row
+          [:div.col-sm-5 [:label "State*"]]
+          [:div.col-sm-7 [myselect]]]]
+        [:div.col-md-5
+         [:div.row {:on-mouse-enter (fn [event] (reset! telbool true))
+                    :on-mouse-leave (fn [event]
+                                      (reset! telbool false))}
+          [:div.col-sm-5 [:label "Tel.(Res)"]]
+          [:div.col-sm-7
+           (if (= @telbool true)
+             [mytextbox1 "tel" telbool teltext]
+             [:span  @teltext])]]]]
+       
+       [:div.row
+        [:div.col-md-5
+         [:div.row {:on-mouse-enter (fn [event] (reset! pinbool true))
+                    :on-mouse-leave (fn [event]
+                                      (reset! pinbool false))}
+          [:div.col-sm-5 [:label "Pincode*"]]
+          [:div.col-sm-7
+           (if (= @pinbool true)
+             [mytextbox1 "pin" pinbool pintext]
+             [:span  @pintext])]]]
+        [:div.col-md-5
+         [:div.row {:on-mouse-enter (fn [event] (reset! faxbool true))
+                    :on-mouse-leave (fn [event]
+                                      (reset! faxbool false))}
+          [:div.col-sm-5 [:label "Fax"]]
+          [:div.col-sm-7
+           (if (= @faxbool true)
+             [mytextbox1 "fax" faxbool faxtext]
+             [:span @faxtext])]]]]
+       ])))
 
-<div class="dropdown">
-<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
-<span class="caret"></span></button>
-<ul class="dropdown-menu">
-<li><a href="#">HTML</a></li>
-<li><a href="#">CSS</a></li>
-<li><a href="#">JavaScript</a></li>
-</ul>
-</div>
-</div>
+
+(defn primarydetails []
+  [:div.form-group [:label [:h2 "Primary Details"]]
+   [:hr]
+   [:hr]
+   [:div.row
+    [:div.col-md-5
+     [:div.row
+     [:div.col-sm-5 [myradiobutton1 "primarydetails" "aadhar" "Aadhaar No"]]
+     [:div.col-sm-7 [myradiobutton1 "primarydetails" "license" "Driving License"]]]]
+    [:div.col-md-5
+     [:div [myradiobutton1 "primarydetails" "pancardno" "Pan Card No"]]]]
+   [:div.row
+    [:div.col-md-5
+     [:div.row
+     [:div.col-sm-5 [myradiobutton1 "primarydetails" "passport" "Passport"]]
+     [:div.col-sm-7  [myradiobutton1 "primarydetails" "telephonebill" "Latest Telephone Bill"]]]]
+    [:div.col-md-5
+     [:div [myradiobutton1 "primarydetails" "bankacc" "LatestBankA/cStatement(last 6 months)"]]]]
+   [:div.row
+    [:div.col-md-5 [labelandtextbox "*Not more than 3 months old.validity/expiry date of proof to be submitted" "date" "expirydate"]]
+    [:div.col-md-5 [:div.row
+                    [:div.col-sm-5
+                     [:label "Upload a Document"]]
+                    [:div.col-sm-7
+                     [:input {:type "file" :name "upload document" :value "Upload Document"}]]]]]])
 
 
-[:div {:class "dropdown"}
- [:button {:class "btn btn-primary dropdown-toggle" :type "button" :data-toggle "dropdown"} "Select Any District"]]
+(defn loanspeciality []
+  [:div.form-group [:label [:h2 "Loan Speciality"]]
+   [:hr]
+   [:hr]
+   [:div.row
+    [:div.col-md-5
+     [:div.row
+      [:div.col-sm-5 [:input {:type "checkbox" :name "Loans" :value "technology"} [:label"Technology Loans"]]]
+      [:div.col-sm-7 [:input {:type "checkbox" :name "Loans" :value "startup"} [:label "Start-up Loans"]]]]]
+    [:div.col-md-5 [:input {:type "checkbox" :name "Loans" :value "international"} [:label"International Business Expansion Loans"]]]]])
+
+(defn finish []
+  [:div.row
+   [:div.col-md-4]
+   [:div.col-md-4 [:input {:type "button" :name "finish" :value "Finish"}]]])
 
 
 
 (defn home []
   [:div.container
    [personaldetails]
-   [:hr]
-   [addressdetails]])
+   [addressdetails]
+   [primarydetails]
+   [loanspeciality]
+   [finish]])
+
+
+
+(defn homeimg []
+  ;; [:img {:src "http://localhost:8081/vinod.jpeg" :width 20 :height 30} ]
+  [:div
+  [:img.img-rounded
+   {:src "http://localhost:8081/vinod.jpg" :alt "vinod pic"
+    :width "100" :height "150"}]])
 
 (reagent/render-component [home]
                           (.getElementById js/document "app"))

bdf0f5f7be1b954bfccf87f0efeb70dfcf9077cd
vinogandamalla <vinodgndmalla@gmail.com>
2016-03-29 18:41:27 +0530

    wrote 3 mock tests authenticate,changepassword, forgotpassword

--- src/clj/loginform/core.clj
+++ src/clj/loginform/core.clj
@@ -16,17 +16,20 @@
 ;; It should return a proper response. In our
 ;; case it returns a content of static index.html.
 
-(def in-db {:users [{:user "user1" :password "techno" :email-id "user1@gmail.com"}
-                   {:user "user2" :password "identity" :email-id "user2@gmail.com"}
-                   {:user "user3" :password "ti" :email-id "user3@gmail.com"}
-                   {:user "user4" :password "tid" :email-id "user4@gmail.com"}]})
+(def in-db (atom {:users [{:user "user1" :password "techno" :email-id "user1@gmail.com"}
+                          {:user "user2" :password "identity" :email-id "user2@gmail.com"}
+                          {:user "user3" :password "ti" :email-id "user3@gmail.com"}
+                          {:user "user4" :password "tid" :email-id "user4@gmail.com"}]}))
+
+
+
 
 
 (defn find-in [in-db email-id]
   (println  email-id)
   (first
    (filter (fn[user](= (:email-id user) email-id))
-           (:users in-db))))
+           (:users @in-db))))
 
 
 (defn home
@@ -36,30 +39,34 @@
 ;; Routes definition
 (defroutes app-routes
   (GET "/" [] home)
+
   (POST "/authenticate" {body :body}
         (let [usr (find-in in-db (:email-id body))]
           (println (:email-id body))
           (rr/content-type
            (cond (or (nil? usr)
-                     (not= (:password usr)(:password body))) (rr/status {:message "invalid credentials"} 404)
+                     (not= (:password usr)(:password body))) (rr/status {:message "invalid credentials"} 401)
                  :else (rr/response (dissoc usr :password))) "application/json")))
  
   (POST "/changepassword" {body :body}
-    (let [usr (find-in in-db (:email-id body))]
-      (println (:email-id body))
+
+    (let [usr (find-in in-db "user3@gmail.com")]
       (rr/content-type
-       (cond (or (nil? usr)
-                 (not= (:password usr)(:password body))) (rr/status {:message "invalid user"} 404)
-             :else (rr/response (dissoc usr :password))) "application/json")))
+       (cond (not= (:password usr)(:password body)) (rr/status {:body {:message "current password does not match"}} 401)
+             (not=(:newpassword body)(:confirmnewpassword body))(rr/status {:body {:message "new password and confirmnewpassword does not match"}} 401)
+             :else ( do
+                    (reset! in-db (assoc @in-db :users
+                                         (map (fn [m]
+                                                (if (=(:email-id m) "user3@gmail.com")(assoc m :password (:newpassword body))
+                                                    m)) (:users @in-db))))
+                    (rr/response {:message "changed successfully"})))"application/json")))
   
 
   (POST "/forgotpassword" {body :body}
-    (let [usr (find-in in-db (:email-id body))]
-      (println (:email-id body))
-      (rr/content-type
-       (cond (or (nil? usr)
-                 (not= (:password usr)(:password body))) (rr/status {:message "invalid user"} 404)
-             :else (rr/response (dissoc usr :password :user))) "application/json")))
+    (let [usr (find-in in-db (:email body))]
+    (rr/content-type
+     (cond (nil? usr) (rr/status {:body {:message "invalid user"}} 401) 
+           :else (rr/response {:message "check  your mail for reset link"})) "application/json")))
   
   (route/resources "/static")
   (route/not-found "<h1>Page not found</h1>"))
